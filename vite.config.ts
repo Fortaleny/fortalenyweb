e101ed9ead47d2be150af427886565c07bc07ead
Doumian <dlarena97@gmail.com>
2025-07-19 17:12:13 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig(({ mode }) => ({
     react(),
     componentTagger(),
   ].filter(Boolean),
-  base:'/fortalenyweb/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
