f2ad6c10b482cb88d862504c76f80c5f3cb4f46e
Doumian <dlarena97@gmail.com>
2025-07-19 17:18:52 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc"; // or "@vitejs/plugin-react"
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 export default defineConfig({
   base: '/fortalenyweb/',
-  plugins: [react()],
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    componentTagger(), // optional: keep this if you're using it
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
